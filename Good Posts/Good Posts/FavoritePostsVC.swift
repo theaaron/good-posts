745384bb87800bf3ff2e5feda8bbc26b968c1189
aaron <4142467+theaaron@users.noreply.github.com>
2022-12-25 23:16:34 -0600

    Initial project setup. Setup navigation bar and created Posts view controller and Bookmarks view Controller.

---
+++ Good Posts/Good Posts/FavoritePostsVC.swift
@@ -0,0 +1,39 @@
+//
+//  FavoritePostsVC.swift
+//  Good Posts
+//
+//  Created by aaron on 12/23/22.
+//
+
+import UIKit
+
+class FavoritePostsVC: UIViewController {
+    
+    let postsTableView = UITableView()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupTableView()
+    }
+    
+    
+    func setupTableView() {
+        view.addSubview(postsTableView)
+        postsTableView.translatesAutoresizingMaskIntoConstraints = false
+        postsTableView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        postsTableView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        postsTableView.leftAnchor.constraint(equalTo: view.leftAnchor).isActive = true
+        postsTableView.rightAnchor.constraint(equalTo: view.rightAnchor).isActive = true
+        postsTableView.backgroundColor = .systemGray6
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+
+}
